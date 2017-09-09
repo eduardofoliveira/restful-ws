c1622df59cb74ec1831005aacf073cea379e7f1a
Eduardo Felipe de Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2017-09-09 15:54:38 -0300

    Adição environment

--- src/services/mysql/index.js
+++ src/services/mysql/index.js
@@ -1,10 +1,10 @@
 const mysqlServer = require('mysql');
 
 const connection = mysqlServer.createConnection({
-    host: 'duduhouse.dyndns.info',
-    user: 'restful',
-    password: '190790edu',
-    database: 'restful_ws'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 const errorHandler = (error, msg, rejectFunction) => {
