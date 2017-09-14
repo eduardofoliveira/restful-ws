e3c48e6aff791c3f200b8d674967ec0fba8d8466
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2017-09-14 11:42:32 -0300

    Adicionado dotenv

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+const fs = require('fs');
+if(fs.existsSync('../.env')){
+    require('dotenv').config({path: '../.env'});
+}else{
+    require('dotenv').config();
+}
+
 const server = require('./server');
 server.listen('80');
